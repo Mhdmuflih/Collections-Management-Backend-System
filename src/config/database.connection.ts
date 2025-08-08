e3314a198b1703c797a912a03482016594474ceb
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-08-08 15:45:37 +0530

    database connection compelted and server created

---
+++ src/config/database.connection.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.mongoURL as string, {dbName: "collectionsApp"});
+        console.log(`MongoDB Connected Successfully: ${connect.connection.host}`);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log("Failed to connect the MongoDB.", error.message);
+        } else {
+            console.log("An unknown error occurred while connecting to MongoDB.");
+        }
+        process.exit(1);
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express, { Application } from "express";
+import dotenv from 'dotenv';
+import cors from "cors";
+import { connectDB } from "./config/database.connection";
+
+// ====================================================
+
+dotenv.config();    // env configuration.
+
+const app: Application = express();
+
+app.use(cors());    //cors set up.
+
+let port: number = Number(process.env.PORT) || 3000;
+const startServer = async () => {
+    await connectDB();
+    app.listen(port, () => {
+        console.log(`Server Is Running on http://localhost${port}`);
+    });
+}
+startServer();
