65615ef26f02aa712a31ce67854a9a2434b73fdf
MUHAMMED MUFLIH K K <151443394+Mhdmuflih@users.noreply.github.com>
2025-08-12 09:13:01 +0530

    loger middleware added and 404 setup completed and morgen third party middleware used

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import dotenv from 'dotenv';
 import cors from "cors";
 
@@ -8,6 +8,8 @@ import Auth_Routes from "./routes/auth.routes";
 import Account_Routes from "./routes/account.routes";
 import Payment_Routes from "./routes/payment.routes";
 import Activity_Routes from "./routes/activity.routes";
+import morgan from "morgan";
+import logger from "./middlewares/logger.middleware";
 // ============================================================
 
 dotenv.config();            // env configuration.
@@ -15,6 +17,24 @@ dotenv.config();            // env configuration.
 const app: Application = express();
 
 app.use(cors());            //cors set up.
+
+app.use(morgan("tiny"));
+
+// Custom logging middleware
+// ============================================================
+app.use((req, res, next) => {
+    logger.info(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+// ============================================================
+
+
+// 404 error throw
+// ============================================================
+app.use("*", (req: Request, res: Response) => {
+    res.status(404).json({ message: "Route not found" });
+});
+// ============================================================
 
 // body parsing
 app.use(express.json());    // convert to json format
